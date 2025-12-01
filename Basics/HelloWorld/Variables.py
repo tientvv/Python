900e6feda07355716232646a6cc84262caec01df
tientvv <tientranvan19052004@gmail.com>
2025-12-01 23:53:15 +0700

    Add introductory Python exercises for Hello World
    Added several beginner Python scripts covering basic concepts such as printing, string concatenation, user input, variable naming, and variable usage. These files serve as foundational exercises for new learners.

---
+++ Basics/HelloWorld/Variables.py
@@ -0,0 +1,12 @@
+name = "Tien"
+print(name)
+
+name = "Tran Van"
+print(name)
+
+# print(len(input("Enter your name: ")))
+print(len(name))
+
+username = input("Enter your name: ")
+length = len(username)
+print(length)
