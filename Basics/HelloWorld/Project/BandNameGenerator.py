089d3e9fde850b25d3e91ebfe8205cbc5f8c0462
tientvv <tientranvan19052004@gmail.com>
2025-12-02 22:20:26 +0700

    Add initial coding exercises for Hello World, including variable naming, debugging practice, and a band name generator

---
+++ Basics/HelloWorld/Project/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of your city you grew up in?\n")
+cat = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + cat)
---
+++ Basics/HelloWorld/VariableNaming.py
@@ -0,0 +1,3 @@
+username_of_this_person = "Tien"
+length_of_username = len(username_of_this_person)
+print(length_of_username)
